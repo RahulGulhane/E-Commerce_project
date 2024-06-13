415125c19143a4d949fe9c378906f3bd72e9d0d8
RahulGulhane <rahulgulhane1998@gmail.com>
2024-06-13 10:47:59 +0530

    commit Start the Controller Lyare

--- src/main/java/com/bikkadit/demo/controller/UserController.java
+++ src/main/java/com/bikkadit/demo/controller/UserController.java
@@ -1,11 +1,34 @@
 package com.bikkadit.demo.controller;
 
 
+import com.bikkadit.demo.model.User;
+import com.bikkadit.demo.service.UserServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class UserController {
+    @Autowired
+    private UserServiceI userServiceI;
 
-    // Rahul Changes
+    //create
+    ResponseEntity<User> createUser(){
+
+        return null;
+    }
+
+
+    //update
+
+    //GET single data
+
+    //GET all data
+
+    //Delete
+
+
 
 }
