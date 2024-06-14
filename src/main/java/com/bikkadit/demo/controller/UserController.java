84d56880692dbd0cd740666f0934d0082152a3bb
RahulGulhane <rahulgulhane1998@gmail.com>
2024-06-14 17:51:10 +0530

    commit compliet the Controller Lyare

--- src/main/java/com/bikkadit/demo/controller/UserController.java
+++ src/main/java/com/bikkadit/demo/controller/UserController.java
@@ -4,9 +4,11 @@ package com.bikkadit.demo.controller;
 import com.bikkadit.demo.model.User;
 import com.bikkadit.demo.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -15,20 +17,36 @@ public class UserController {
     private UserServiceI userServiceI;
 
     //create
-    ResponseEntity<User> createUser(){
-
-        return null;
+    @PostMapping
+    ResponseEntity<User> createUser(User user){
+        User saveuser = userServiceI.createUser(user);
+        return new ResponseEntity<>(saveuser, HttpStatus.CREATED);
     }
 
 
-    //update
-
-    //GET single data
-
-    //GET all data
-
+    //GetAllUsers
+    @GetMapping("/users")
+    ResponseEntity<List<User>> getAllUser(){
+        List<User> allUsers = userServiceI.getAllUser();
+       return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+    //GET single user
+    @GetMapping("/users/{UserId}")
+    public ResponseEntity<User> getSingleUser(@PathVariable long userId){
+        User user = userServiceI.getsingleUser(userId);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+    //updateUser
+    @PutMapping("/users/{UserId}")
+    public ResponseEntity<User> updateUser(@RequestBody User user,@PathVariable Long userId){
+        User updateuser = userServiceI.updateUser(user, userId);
+        return new ResponseEntity<>(updateuser,HttpStatus.OK);
+    }
     //Delete
-
+    public ResponseEntity deleteUser(@PathVariable Long userId){
+        userServiceI.deleteUser(userId);
+        return new ResponseEntity<>("Resource Delete Successful ",HttpStatus.OK);
+    }
 
 
 }
