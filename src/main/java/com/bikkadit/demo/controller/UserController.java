6e1c8030dbf3d7745c3711817339eada59517432
RahulGulhane <rahulgulhane1998@gmail.com>
2024-06-14 18:52:43 +0530

    changes in update user API

--- src/main/java/com/bikkadit/demo/controller/UserController.java
+++ src/main/java/com/bikkadit/demo/controller/UserController.java
@@ -18,37 +18,63 @@ public class UserController {
 
     //create
     @PostMapping("/Users")
-    ResponseEntity<User> createUser(User user){
+    public ResponseEntity<User> create(User user){
         User saveuser = userServiceI.createUser(user);
         return new ResponseEntity<>(saveuser, HttpStatus.CREATED);
     }
+//    @PostMapping("/Users")
+//    ResponseEntity<User> createUser(User user){
+//        User saveuser = userServiceI.createUser(user);
+//        return new ResponseEntity<>(saveuser, HttpStatus.CREATED);
+//    }
 
 
     //GetAllUsers
-    @GetMapping("/users")
-    ResponseEntity<List<User>> getAllUser(){
-        List<User> allUsers = userServiceI.getAllUser();
-       return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    @GetMapping("/Users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> allUser = userServiceI.getAllUser();
+        return new ResponseEntity<>(allUser, HttpStatus.OK);
     }
+//    @GetMapping("/users")
+//    ResponseEntity<List<User>> getAllUser(){
+//        List<User> allUsers = userServiceI.getAllUser();
+//       return new ResponseEntity<>(allUsers, HttpStatus.OK);
+//    }
     //GET single user
-    @GetMapping("/users/{UserId}")
-    public ResponseEntity<User> getSingleUser(@PathVariable long userId){
-        User user = userServiceI.getsingleUser(userId);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    @GetMapping("/users/{UsersId}")
+    public ResponseEntity<User> getsinglUser(@PathVariable Long userId){
+        User user1 = userServiceI.getsingleUser(userId);
+        return new ResponseEntity<>(user1, HttpStatus.OK);
     }
+
+//    @GetMapping("/users/{UserId}")
+//    public ResponseEntity<User> getSingleUser(@PathVariable long userId){
+//        User user = userServiceI.getsingleUser(userId);
+//        return new ResponseEntity<>(user, HttpStatus.CREATED);
+//    }
     //updateUser
-    @PutMapping("/users/{UserId}")
+    @PutMapping("/users/{userId}")
     public ResponseEntity<User> updateUser(@RequestBody User user,@PathVariable Long userId){
-        User updateuser = userServiceI.updateUser(user, userId);
-        return new ResponseEntity<>(updateuser,HttpStatus.OK);
+        User updateUser = userServiceI.updateUser(user, userId);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
+//    @PutMapping("/users/{UserId}")
+//    public ResponseEntity<User> updateUser(@RequestBody User user,@PathVariable Long userId){
+//        User updateuser = userServiceI.updateUser(user, userId);
+//        return new ResponseEntity<>(updateuser,HttpStatus.OK);
+//    }
     //Delete
-    public ResponseEntity deleteUser(@PathVariable Long userId){
+    @DeleteMapping("/users/{UserId}")
+    public ResponseEntity Delete(Long userId){
         userServiceI.deleteUser(userId);
-        return new ResponseEntity<>("Resource Delete Successful ",HttpStatus.OK);
-    }
+        return ResponseEntity.status(HttpStatus.OK).body("Resource Delete Successful");
+    }//  OR
+//    public ResponseEntity deleteUser(@PathVariable Long userId){
+//        userServiceI.deleteUser(userId);
+//        return new ResponseEntity<>("Resource Delete Successful ",HttpStatus.OK);
+//    }
 
 
-    // new changes
+
 
 }
