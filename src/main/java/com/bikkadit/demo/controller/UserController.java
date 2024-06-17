a51ef8ba838219a6581c31bf57bf8de2cf2d7712
RahulGulhane <rahulgulhane1998@gmail.com>
2024-06-17 15:18:09 +0530

    add the log Service and Dao lyare

--- src/main/java/com/bikkadit/demo/controller/UserController.java
+++ src/main/java/com/bikkadit/demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.bikkadit.demo.controller;
 
 import com.bikkadit.demo.model.User;
 import com.bikkadit.demo.service.UserServiceI;
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +16,18 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class UserController {
+
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserServiceI userServiceI;
 
     //create
     @PostMapping("/Users")
     public ResponseEntity<User> create(User user){
+        logger.info("Entering the request for save user data");
         User saveuser = userServiceI.createUser(user);
+        logger.info("compete the request for save user data");
         return new ResponseEntity<>(saveuser, HttpStatus.CREATED);
     }
 //    @PostMapping("/Users")
@@ -32,7 +40,9 @@ public class UserController {
     //GetAllUsers
     @GetMapping("/Users")
     public ResponseEntity<List<User>> getAllUsers(){
+        logger.info("Entering the request for  getAlluser data");
         List<User> allUser = userServiceI.getAllUser();
+        logger.info("Completed the request for getalluser data with  userId");
         return new ResponseEntity<>(allUser, HttpStatus.OK);
     }
 //    @GetMapping("/users")
@@ -43,7 +53,9 @@ public class UserController {
     //GET single user
     @GetMapping("/users/{UsersId}")
     public ResponseEntity<User> getsinglUser(@PathVariable Long userId){
+        logger.info("Entering the request for getsingleuser data with userId {}:",userId);
         User user1 = userServiceI.getsingleUser(userId);
+        logger.info("Completed the request for getsingluser data with  userId {}:",userId);
         return new ResponseEntity<>(user1, HttpStatus.OK);
     }
 
@@ -55,7 +67,9 @@ public class UserController {
     //updateUser
     @PutMapping("/users/{userId}")
     public ResponseEntity<User> updateUser(@RequestBody User user,@PathVariable Long userId){
+        logger.info("Entering the request for update for update the user data with userId {}:",userId);
         User updateUser = userServiceI.updateUser(user, userId);
+        logger.info("Completed the request for updatedata with  userId {}:",userId);
         return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
 //    @PutMapping("/users/{UserId}")
@@ -66,7 +80,9 @@ public class UserController {
     //Delete
     @DeleteMapping("/users/{UserId}")
     public ResponseEntity Delete(Long userId){
+        ogger.info("Entering the request for delete for delete the user data with userId {}:",userId);
         userServiceI.deleteUser(userId);
+        logger.info("Completed the request for delete with  userId {}:",userId);
         return ResponseEntity.status(HttpStatus.OK).body("Resource Delete Successful");
     }//  OR
 //    public ResponseEntity deleteUser(@PathVariable Long userId){
--- src/main/java/com/bikkadit/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/bikkadit/demo/service/Impl/UserServiceImpl.java
@@ -3,6 +3,9 @@ package com.bikkadit.demo.service.Impl;
 import com.bikkadit.demo.model.User;
 import com.bikkadit.demo.repository.UserRepository;
 import com.bikkadit.demo.service.UserServiceI;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
@@ -11,14 +14,20 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserServiceI {
 
+
+  //  Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserRepository userRepository;
     //create User
     @Override
     public User createUser(User user) {
+
+        log.info("Intiating the dao call for save user data");
         User saveUser = userRepository.save(user);
+        log.info("Completed the dao call for save user data");
         return saveUser;
     }
         //update User
@@ -34,8 +43,10 @@ public class UserServiceImpl implements UserServiceI {
         //GET single data
     @Override
     public User getsingleUser(Long userId) {
-
+        log.info("Intiating the dao call for  user get single user data userId {}:",userId);
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resources Not Found on Server"+ userId));
+
+        log.info("Intiating the dao call for get single  user data as userId {}:",userId);
         return user;
         /* OR */
 //        Optional<User> user = userRepository.findById(userId);
@@ -57,7 +68,10 @@ public class UserServiceImpl implements UserServiceI {
         //Delete User
     @Override
     public void deleteUser(Long userId) {
+
+        log.info("Intiating the dao call for  user get single user data userId {}:",userId);
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException(("Resource Not Found on server" + userId)));
+        log.info(" Complete the dao call for Deleting  user data as userId {}:",userId);
         userRepository.delete(user);
     }
 }
